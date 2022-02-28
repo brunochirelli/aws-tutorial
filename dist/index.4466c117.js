9b1ce85915a4bea3a2b17488d6ac4c04cc527a0d
Bruno Chirelli <brunochirelli@gmail.com>
2022-02-28 11:46:34 -0300

    Fix parcel build

---
+++ dist/index.4466c117.js
@@ -0,0 +1,2 @@
+document.addEventListener("readystatechange",(function(){const e=new Date,t=e.getFullYear(),n=e.getMonth(),d=e.getDate(),a=document.querySelector("body"),o=document.createElement("div");o.innerHTML=`Hey! Today is ${d}/${n}/${t}`,a.appendChild(o)}));
+//# sourceMappingURL=index.4466c117.js.map
