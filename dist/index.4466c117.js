f5280560d2653e0c6b945df37ef8ad4a45940a05
Bruno Chirelli <brunochirelli@gmail.com>
2022-02-28 22:20:19 -0300

    Update repo deploy settings

--- dist/index.4466c117.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("readystatechange",(function(){const e=new Date,t=e.getFullYear(),n=e.getMonth(),d=e.getDate(),a=document.querySelector("body"),o=document.createElement("div");o.innerHTML=`Hey! Today is ${d}/${n}/${t}`,a.appendChild(o)}));
-//# sourceMappingURL=index.4466c117.js.map
